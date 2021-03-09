2ef4f16a4439cc0b23aa7c4d4c233d98f051465d
Reno Paslah <reno.nilam@gmail.com>
2021-03-09 08:18:04 +0700

    Fix (UploadFile) Menampilkan filename when upload

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -69,6 +69,13 @@ $('.goToSelectedUrl').on('change', function() {
 $('.date').datepicker({
     dateFormat: "dd-mm-yy"
 });
+
+$('#customFile').on('change', function() {
+    // Ambil nama file 
+    let fileName = $(this).val().split('\\').pop();
+    // Ubah "Choose a file" label sesuai dengan nama file yag akan diupload
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
+});
 </script>
 </body>
 
